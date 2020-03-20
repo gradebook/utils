c26096a0c309a08387f37cf52770cd64ef19825f
Vikas Potluri <vikaspotluri123.github@gmail.com>
2020-03-20 13:30:01 -0500

    fix missing schema refs

--- packages/importer/src/schema/index.ts
+++ packages/importer/src/schema/index.ts
@@ -1,9 +1,14 @@
 import v0 from './v0.json';
+import cut from './cut.json';
+import cutName from './cut-name.json';
+
 import course from './course.json';
 import category from './category.json';
 import grade from './grade.json';
 
 export const SCHEMAS = [
+	cut,
+	cutName,
 	grade,
 	category,
 	course,
