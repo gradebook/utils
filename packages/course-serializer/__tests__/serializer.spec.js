3bf89408bcf9070e73223711de4700d17cd532a0
Josh Costa <joshcos97@gmail.com>
2020-04-20 14:06:21 -0500

    update tests to reflect new schema

--- packages/course-serializer/__tests__/serializer.spec.js
+++ packages/course-serializer/__tests__/serializer.spec.js
@@ -5,7 +5,7 @@ const serializer = require('../lib/course-serializer');
 const getSafeCourse = () => JSON.parse(JSON.stringify(require('../fixtures/example-serialized-course')));
 const getExampleCourse = () => JSON.parse(JSON.stringify(require('../fixtures/example-course')));
 
-const SERIALIZED_COURSE = 'eyJtIjoiMHwyMDIwfDB8OTB8ODB8NzB8NjB8RXhhbXBsZSBDb3Vyc2UiLCJ6IjpbIjB8MHwyMHwwfEF0dGVuZGFuY2UiLCIwfDB8MTN8MXxPbmxpbmUgUXVpenplcyIsIjB8MHw0fDV8Q3VsdHVyYWwgRGlzY291cnNlIEpvdXJuYWwiLCIxfDB8MXwyMHxFeGFtIDEiLCIxfDB8MXwyMHxFeGFtIDIiLCIxfDB8MXw1fEluaXRpYWwgRXNzYXkiLCIxfDB8MXwxNXxGaW5hbCBFc3NheSIsIjB8MHwyfDEwfFNob3J0IFBhcGVyIiwiMXwwfDF8NXxQYXJ0aWNpcGF0aW9uIiwiMXwwfDF8NXxQcmVzZW50YXRpb24iLCIxfDB8MXwxNXxPbmxpbmUgUXVpenplcyBhZnRlciBkcm9wcGVkIl19';
+const SERIALIZED_COURSE = 'eyJtIjoiMHwyMDIwfDB8OTB8ODB8NzB8NjB8QXxCfEN8RHxFeGFtcGxlIENvdXJzZSIsInoiOlsiMHwwfDIwfDB8QXR0ZW5kYW5jZSIsIjB8MHwxM3wxfE9ubGluZSBRdWl6emVzIiwiMHwwfDR8NXxDdWx0dXJhbCBEaXNjb3Vyc2UgSm91cm5hbCIsIjF8MHwxfDIwfEV4YW0gMSIsIjF8MHwxfDIwfEV4YW0gMiIsIjF8MHwxfDV8SW5pdGlhbCBFc3NheSIsIjF8MHwxfDE1fEZpbmFsIEVzc2F5IiwiMHwwfDJ8MTB8U2hvcnQgUGFwZXIiLCIxfDB8MXw1fFBhcnRpY2lwYXRpb24iLCIxfDB8MXw1fFByZXNlbnRhdGlvbiIsIjF8MHwxfDE1fE9ubGluZSBRdWl6emVzIGFmdGVyIGRyb3BwZWQiXX0=';
 
 describe('Unit > Serializer', function () {
 	it('strip properly removes PII from a category', function () {
