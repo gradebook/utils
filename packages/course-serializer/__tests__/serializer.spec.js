ad5546858c82c13c8be9019a2350e7bedc1bd889
Josh Costa <joshcos97@gmail.com>
2020-04-26 23:53:34 -0500

    fix tests

--- packages/course-serializer/__tests__/serializer.spec.js
+++ packages/course-serializer/__tests__/serializer.spec.js
@@ -2,10 +2,10 @@
 const {expect} = require('chai');
 const serializer = require('../lib/course-serializer');
 
-const getSafeCourse = () => JSON.parse(JSON.stringify(require('../fixtures/example-serialized-course')));
-const getExampleCourse = () => JSON.parse(JSON.stringify(require('../fixtures/example-course')));
+const getSafeCourse = () => JSON.parse(JSON.stringify(require('../fixtures/example-serialized-course.json')));
+const getExampleCourse = () => JSON.parse(JSON.stringify(require('../fixtures/example-course.json')));
 
-const SERIALIZED_COURSE = 'eyJtIjoiMHwyMDIwfDB8OTB8ODB8NzB8NjB8QXxCfEN8RHxFeGFtcGxlIENvdXJzZSIsInoiOlsiMHwwfDIwfDB8QXR0ZW5kYW5jZSIsIjB8MHwxM3wxfE9ubGluZSBRdWl6emVzIiwiMHwwfDR8NXxDdWx0dXJhbCBEaXNjb3Vyc2UgSm91cm5hbCIsIjF8MHwxfDIwfEV4YW0gMSIsIjF8MHwxfDIwfEV4YW0gMiIsIjF8MHwxfDV8SW5pdGlhbCBFc3NheSIsIjF8MHwxfDE1fEZpbmFsIEVzc2F5IiwiMHwwfDJ8MTB8U2hvcnQgUGFwZXIiLCIxfDB8MXw1fFBhcnRpY2lwYXRpb24iLCIxfDB8MXw1fFByZXNlbnRhdGlvbiIsIjF8MHwxfDE1fE9ubGluZSBRdWl6emVzIGFmdGVyIGRyb3BwZWQiXX0=';
+const SERIALIZED_COURSE = 'eyJtIjoiMHwyMDIwfDB8OTB8ODB8NzB8NjB8QXxCfEN8RHxFeGFtcGxlIENvdXJzZSIsInoiOlsiMXwwfDIwfDB8QXR0ZW5kYW5jZSIsIjF8MHwxM3wxfE9ubGluZSBRdWl6emVzIiwiMXwwfDR8NXxDdWx0dXJhbCBEaXNjb3Vyc2UgSm91cm5hbCIsIjB8MHwxfDIwfEV4YW0gMSIsIjB8MHwxfDIwfEV4YW0gMiIsIjB8MHwxfDV8SW5pdGlhbCBFc3NheSIsIjB8MHwxfDE1fEZpbmFsIEVzc2F5IiwiMXwwfDJ8MTB8U2hvcnQgUGFwZXIiLCIwfDB8MXw1fFBhcnRpY2lwYXRpb24iLCIwfDB8MXw1fFByZXNlbnRhdGlvbiIsIjB8MHwxfDE1fE9ubGluZSBRdWl6emVzIGFmdGVyIGRyb3BwZWQiXX0=';
 
 describe('Unit > Serializer', function () {
 	it('strip properly removes PII from a category', function () {
