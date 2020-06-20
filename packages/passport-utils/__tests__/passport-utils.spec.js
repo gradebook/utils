20f191b49cb4f7e7aeb035dcd860e02a6a863027
Vikas Potluri <vikaspotluri123.github@gmail.com>
2020-06-19 21:30:55 -0500

    [passport-utils]: initial commit

---
+++ packages/passport-utils/__tests__/passport-utils.spec.js
@@ -0,0 +1,170 @@
+// @ts-check
+const {promisify} = require('util');
+const {expect} = require('chai');
+const sinon = require('sinon');
+
+const _module = require('../lib/passport-utils');
+const {IncomingMessage} = require('http');
+const makeFakeProfile = () => ({
+	id: 'this is a gid',
+	emails: [{value: 'test@gbdev.cf'}],
+	displayName: 'Aggie',
+	name: {
+		givenName: 'Texas A&M',
+		familyName: 'University'
+	}
+});
+
+/** @returns {_module.BasicRequest} */
+const makeFakeMessage = (session = {}) => {
+	// @ts-ignore
+	return Object.assign(new IncomingMessage(null), session);
+};
+
+describe('Unit > Passport Utils', function () {
+	describe('createProfileHandler', function () {
+		let userFunction;
+		let instance;
+
+		beforeEach(function () {
+			userFunction = sinon.stub().resolves();
+			instance = promisify(_module.createProfileHandler(userFunction));
+		});
+
+		it('returns the user provided by getUser()', async function () {
+			const fakeUser = {};
+			userFunction.resolves(fakeUser);
+
+			const response = await instance({}, null, null, {id: 'nope'});
+
+			expect(response).to.equal(fakeUser);
+		});
+
+		it('gracefully fails when getUser() fails', async function () {
+			const expectedError = new Error('oops');
+			userFunction.rejects(expectedError);
+
+			try {
+				await instance({}, null, null, {id: 'nope'});
+				throw new Error('Expect instance to throw');
+			} catch (error) {
+				expect(error).to.equal(expectedError);
+			}
+		});
+
+		// @todo(vikaspotluri123): for joshcosta - Add tests for remaining types (engineering#3)
+		it('stores the proper first and last name: all fields are provided', async function () {
+			const session = {};
+			const profile = makeFakeProfile();
+			userFunction.resolves(null);
+
+			const user = await instance({
+				session
+			}, null, null, profile);
+
+			expect(session.userProfile).to.be.ok;
+			expect(user).to.equal(session.userProfile);
+			expect(session.userProfile.firstName).to.equal('Texas A&M');
+			expect(session.userProfile.lastName).to.equal('University');
+		});
+	});
+
+	describe('createUserDeserializer', function () {
+		let deserialize;
+		let getUser;
+
+		beforeEach(function () {
+			getUser = sinon.stub();
+			deserialize = promisify(_module.createUserDeserializer(getUser, null));
+		});
+
+		it('gracefully handles getUser() failing', async function () {
+			const expectedError = new Error('oops');
+			getUser.rejects(expectedError);
+
+			try {
+				await deserialize(makeFakeMessage({_table: 'school', session: {}}), 'school:id');
+			} catch (error) {
+				expect(error).to.equal(expectedError);
+				expect(getUser.calledWithExactly('id', 'school')).to.be.true;
+			}
+		});
+
+		it('successfully fails when the user does not exist', async function () {
+			getUser.resolves(null);
+
+			const user = await deserialize(makeFakeMessage({_table: 'school', session: {}}), 'school:id');
+
+			expect(user).to.equal(null);
+		});
+
+		it('returns the user under normal circumstances', async function () {
+			const expectedUser = {};
+			getUser.resolves(expectedUser);
+
+			const user = await deserialize(makeFakeMessage({_table: 'school', session: {}}), 'school:id');
+
+			expect(user).to.equal(expectedUser);
+		});
+
+		it('redirects the user when they are on the incorrect domain', async function () {
+			deserialize = promisify(_module.createUserDeserializer(getUser, 'gbdev.cf'));
+
+			const request = makeFakeMessage({_table: 'differentSchool', session: {}});
+
+			const user = await deserialize(request, 'school:id');
+
+			expect(user).to.be.an('object').and.not.keys;
+			expect(request._passportRedirect).to.be.a('string').and.equal('//school.gbdev.cf/my/');
+		});
+
+		it('handles new profiles', async function () {
+			const userProfile = makeFakeProfile();
+
+			const user = await deserialize(makeFakeMessage({session: {userProfile}}), '__school__:1234');
+
+			expect(user).to.equal(userProfile);
+		});
+	});
+
+	it('createSerializer', async function () {
+		const serialize = promisify(_module.serializeUser);
+
+		/** @type {object} */
+		const profile = {
+			name: 'user'
+		};
+
+		try {
+			await serialize(makeFakeMessage(), profile);
+			throw new Error('Expected failure');
+		} catch (error) {
+			expect(error.message, 'unknown profile schema').to.contain('Unknown profile');
+			expect(error.context).to.equal(profile);
+		}
+
+		profile.id = 'test';
+		expect(await serialize(makeFakeMessage(), profile), 'server with no host matching').to.equal(`null:${profile.id}`);
+
+		profile.isNew = true;
+		expect(await serialize(makeFakeMessage(), profile), 'new user').to.equal(`__school__:${profile.id}`);
+
+		profile.isNew = false;
+
+		expect(
+			await serialize(makeFakeMessage({_table: 'school'}), profile),
+			'server with host matching'
+		).to.equal(`school:${profile.id}`);
+
+		delete profile.id;
+
+		profile.school = 'school';
+		// eslint-disable-next-line camelcase
+		profile.school_id = 'id';
+
+		expect(
+			await serialize(makeFakeMessage(), profile),
+			'auth'
+		).to.equal('school:id');
+	});
+});
---
+++ packages/passport-utils/src/passport-utils.ts
@@ -0,0 +1,158 @@
+import date from 'dayjs';
+import objectId from 'bson-objectid';
+import {IncomingMessage} from 'http';
+
+import {
+	PassportOauth20Profile as Profile,
+	PassportOauth20VerifyCallback as VerifyCallback
+} from './_passport-types';
+
+export type NewUserSessionProfile = {
+	id: string;
+	gid: string;
+	firstName: string;
+	lastName: string;
+	email: string;
+	isNew: true;
+	settings: string;
+};
+
+export type UserProfile = {
+	id: string;
+	isNew: boolean;
+} | {
+	school: string;
+	school_id: string; // eslint-disable-line camelcase
+};
+
+export interface BasicRequest extends IncomingMessage {
+	session: {
+		userProfile?: NewUserSessionProfile;
+	};
+	_table?: string;
+	_passportRedirect?: string;
+}
+
+export type BasicCallback<T = string> = (error?: Error, id?: T) => void;
+
+export function createProfileHandler(getUser: (gid: string, table: string) => Promise<UserProfile>) {
+	return async function handleProfile(
+		request: BasicRequest,
+		_: string,
+		__: string,
+		profile: Profile,
+		callback: VerifyCallback
+	): Promise<void> {
+		let user: object | NewUserSessionProfile;
+
+		try {
+			user = await getUser(profile.id, request._table);
+
+			if (user) {
+				callback(null, user);
+				return;
+			}
+		} catch (error) {
+			callback(error);
+			return;
+		}
+
+		const {id: gid, emails, displayName, name: {givenName: firstName, familyName: lastName}} = profile;
+		let firstNameFallback = displayName.slice(0, displayName.indexOf(' '));
+		let lastNameFallback = displayName.slice(displayName.indexOf(' ') + 1);
+
+		if (!firstNameFallback || !lastNameFallback) {
+			firstNameFallback = 'Student';
+			lastNameFallback = '';
+		}
+
+		const id = objectId.generate();
+		user = {
+			id,
+			gid,
+			firstName: firstName || firstNameFallback,
+			lastName: lastName || lastNameFallback,
+			email: emails[0].value,
+			isNew: true,
+			// https://github.com/tgriesser/knex/issues/2649
+			settings: JSON.stringify({
+				tour: false,
+				previous_notification: date().format('YYYY-MM-DDTHH:mm:ss.000-06:00') // eslint-disable-line camelcase
+			})
+		};
+
+		request.session.userProfile = user as NewUserSessionProfile;
+
+		// NOTE: serialization occurs with what is provided in the callback!
+		callback(null, user);
+	};
+}
+
+export function createUserDeserializer(
+	getUser: (id: string, school: string) => Promise<UserProfile>,
+	domain: string = null
+): (request: BasicRequest, profile: string, callback: BasicCallback<UserProfile | object>) => Promise<void> {
+	domain = domain?.replace(/^\./, '');
+
+	return async function deserializeUser(
+		request: BasicRequest,
+		profile: string,
+		callback: BasicCallback<UserProfile | object>
+	): Promise<void> {
+		// CASE: user has not approved their account
+		if (request.session.userProfile) {
+			return callback(null, request.session.userProfile);
+		}
+
+		if (!profile.includes(':')) {
+			return callback(new Error(`Deserializer: Unable to parse profile: ${profile}`));
+		}
+
+		const [school, id] = profile.split(':');
+
+		if (domain && school !== 'null' && school !== request._table) {
+			request._passportRedirect = `//${school}.${domain}/my/`;
+			return callback(null, {});
+		}
+
+		try {
+			const user = await getUser(id, school);
+
+			if (!user) {
+				return callback(null, null);
+			}
+
+			return callback(null, user);
+		} catch (error) {
+			return callback(error);
+		}
+	};
+}
+
+export function serializeUser(
+	request: BasicRequest,
+	profile: UserProfile,
+	callback: BasicCallback
+): void {
+	// CASE: full user object
+	if ('id' in profile) {
+		// CASE: User is new so return the special school identifier
+		if (profile.isNew) {
+			return callback(null, `__school__:${profile.id}`);
+		}
+
+		const table = request._table || null;
+
+		return callback(null, `${table}:${profile.id}`);
+	}
+
+	// CASE: user reference object
+	if ('school' in profile && 'school_id' in profile) {
+		return callback(null, `${profile.school}:${profile.school_id}`);
+	}
+
+	const error = new Error('Serializer: Unknown profile');
+	// @ts-ignore
+	error.context = profile;
+	return callback(error);
+}
