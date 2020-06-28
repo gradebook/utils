eedbefe45271539f8c59effca3cf42df8ebd740f
Vikas Potluri <vikaspotluri123.github@gmail.com>
2020-06-27 22:38:30 -0500

    wip: add support for multiple compilation targets

--- README.md
+++ README.md
@@ -14,3 +14,23 @@ Each package has common code-quality scripts:
 - `test:coverage` to run tests with test coverage enabled. We don't have CI coverage enabled at the moment
 
 The code for each package is written in the `src` folder, and compiled to the `lib` folder
+
+## Compiling for multiple Module Types (targets)
+
+As we are unable to exclusively use ES Modules until at least Node 14.x (becomes LTS), some packages will require compilation to multiple module types.
+
+The standard module type is CommonJS. The `tsconfig.json` file in each package should compile to that.
+
+### Adding additional compilation targets
+
+1. Add the additional typescript configurations with the filename `tsconfig.{type}.json`
+
+	 - In each tsconfig, the output folder should be `lib/{target}` - e.g. `lib/commonjs` or `lib/module`
+
+1. Ensure the `main` and `module` keys exist in `package.json`. Main should reference CommonJS and
+
+1. Update the `prepublish` script to run `../../scripts/compile-all.js`
+
+1. Add the `targets` property to `package.json` with a list of *additional* tsconfig projects
+
+	- If you want to compile to CommonJS and ESModule, the value would be `["module"]` assuming the secondary config file is `tsconfig.module.json` - the `compile-all.js` will run tsc using `tsconfig.json` and `tsconfig.module.json`
---
+++ scripts/compile-all.js
@@ -0,0 +1,78 @@
+// @ts-check
+const {resolve} = require('path');
+const {readdir} = require('fs').promises;
+const execa = require('execa');
+const chalk = require('chalk').default;
+
+const tsc = resolve(__dirname, '../node_modules/.bin/tsc');
+
+/**
+ * @param {object} packageJSON
+ * @returns {string[]}
+ */
+function determineCompileTargets(packageJSON) {
+	if (!Reflect.has(packageJSON, 'targets') || !Array.isArray(packageJSON.targets)) {
+		console.error(
+			chalk.red('Compilation failed! package.json does not contain an array of targets to compile to')
+		);
+
+		process.exit(1);
+	}
+
+	const projects = ['tsconfig.json'];
+
+	packageJSON.targets.forEach(target => {
+		projects.push(`tsconfig.${target}.json`);
+	});
+
+	return projects;
+}
+
+/**
+ * @param {string} project tsconfig project to use
+ */
+async function compileProject(project) {
+	return execa('node', [tsc, '-p', project], {stdio: 'inherit'});
+}
+
+async function failIfLibContainsIllegalFiles() {
+	try {
+		const fileContents = await readdir(resolve(process.cwd(), 'lib'));
+
+		for (const file of fileContents) {
+			if (file.includes('.')) {
+				console.error(
+					chalk.red('The output folder (lib) contains an illegal file (') +
+					file + chalk.red(').'),
+					'Please remove the entire lib folder and run this command again'
+				);
+
+				process.exit(1);
+			}
+		}
+	} catch (error) {
+		if (error.code === 'ENOENT') {
+			return;
+		}
+
+		console.error('An unknown error occurred');
+		console.error(error);
+		process.exit(1);
+	}
+}
+
+async function run() {
+	await failIfLibContainsIllegalFiles();
+	const pkg = require(resolve(process.cwd(), 'package.json'));
+	const projects = determineCompileTargets(pkg);
+
+	const totalPackages = projects.length;
+	let index = 1;
+
+	for (const project of projects) {
+		console.log(chalk.cyan('Compiling target %d/%d (%s)'), index++, totalPackages, project);
+		await compileProject(project); // eslint-disable-line no-await-in-loop
+	}
+}
+
+run();
