b8459dedc90d04485db9b9b3881f5a092c9c5f5c
Josh Costa <joshcos97@gmail.com>
2020-07-17 21:50:32 -0500

    remove v0 course interface

--- packages/course-serializer/src/interfaces/course.ts
+++ packages/course-serializer/src/interfaces/course.ts
@@ -1,19 +1,5 @@
 import {Category} from './category';
 
-export interface Iv0Course {
-	name: string;
-	credits: number;
-	cut1: number;
-	cut1Name: string;
-	cut2: number;
-	cut2Name: string;
-	cut3: number;
-	cut3Name: string;
-	cut4: number;
-	cut4Name: string;
-	categories: Category[];
-}
-
 export interface Course {
 	name: string;
 	credits: number;
