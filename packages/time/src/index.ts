8cffa17f7311b85b70d8dc6d9f1ad5a65d826db7
Vikas Potluri <vikaspotluri123.github@gmail.com>
2020-09-03 20:29:31 -0500

    :bug: expose unitsToMs

--- packages/time/src/index.ts
+++ packages/time/src/index.ts
@@ -4,3 +4,4 @@ import cpf from 'dayjs/plugin/customParseFormat';
 export const dayjs = dayjs_.extend(cpf);
 export * as semester from './semester';
 export * as tzOffset from './tz-offset';
+export {default as unitsToMs} from './units-to-ms';
