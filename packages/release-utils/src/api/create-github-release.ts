61c6a24b0b7ed45c7305b885d5eea560969d358c
Vikas Potluri <vikaspotluri123.github@gmail.com>
2021-02-08 01:34:24 -0600

    bootstrap release-utils/create-github-release (no assets)

---
+++ packages/release-utils/src/api/create-github-release.ts
@@ -0,0 +1,71 @@
+import got from 'got';
+
+interface GitHubPutReleaseBody {
+	tag_name: string; // eslint-disable-line camelcase
+	target_commitish?: string; // eslint-disable-line camelcase
+	name?: string;
+	body?: string;
+	draft?: boolean;
+	prerelease?: boolean;
+}
+
+// Partial
+interface GithubPutReleaseResponse {
+	url: string;
+	id: number;
+}
+
+export interface CreateGitHubReleaseOptions {
+	token: string;
+	owner: string;
+	repo: string;
+	referenceTag: string;
+	draft?: boolean;
+	name?: string;
+	body?: string;
+	prerelease?: boolean;
+}
+
+export default async function createGitHubReleaseFromExistingTag({
+	token,
+	owner,
+	repo,
+	referenceTag,
+	draft = true,
+	name,
+	body: releaseBody,
+	prerelease = false
+}: CreateGitHubReleaseOptions, assets: string[] = []): Promise<unknown> {
+	const url = `https://api.github.com/repos/${owner}/${repo}/releases`;
+	const json: GitHubPutReleaseBody = {
+		tag_name: referenceTag, // eslint-disable-line camelcase
+		draft,
+		prerelease
+	};
+
+	if (name !== undefined) {
+		json.name = name;
+	}
+
+	if (releaseBody !== undefined) {
+		json.body = releaseBody;
+	}
+
+	const {body} = await got<GithubPutReleaseResponse>({
+		url,
+		method: 'post',
+		json,
+		headers: {
+			accept: 'application/vnd.github.v3+json',
+			authorization: `token ${token}`
+		}
+	});
+
+	if (assets.length === 0) {
+		return body.id;
+	}
+
+	for (const asset of assets) {
+		// @TODO
+	}
+}
