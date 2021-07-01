aae41013339f87a5b0395e78bf598df2334a131e
Vikas Potluri <vikaspotluri123.github@gmail.com>
2021-06-30 22:00:48 -0500

    allow release-specific test suite

--- packages/release-utils/src/recipes/test-and-publish.ts
+++ packages/release-utils/src/recipes/test-and-publish.ts
@@ -3,10 +3,14 @@ import {configureForRelease, PackageJson as MinimalPackageJson} from '../api/con
 import {getShaFromEnvironment} from '../api/get-sha-from-env.js';
 import {publishPackage} from '../api/publish-package.js';
 
+const SPECIAL_SCRIPT = 'autorelease:test';
+const FALLBACK_SCRIPT = 'test';
+
 interface PackageJson extends MinimalPackageJson {
 	scripts?: {
 		prepublish?: string;
-		'test::for-release'?: string;
+		[SPECIAL_SCRIPT]?: string;
+		[FALLBACK_SCRIPT]?: string;
 	};
 }
 
@@ -16,7 +20,18 @@ async function wrap() {
 
 	await $`yarn install --frozen-lockfile`;
 	await $`yarn lint`;
-	await $`yarn test`;
+
+	if (!packageJson.scripts?.[SPECIAL_SCRIPT] && !packageJson.scripts?.[FALLBACK_SCRIPT]) {
+		console.error('Package does not contain a test script');
+		console.error(`Ensure .scripts.${SPECIAL_SCRIPT} or .scripts.${FALLBACK_SCRIPT} exists`);
+		// eslint-disable-next-line unicorn/no-process-exit
+		process.exit(1);
+	}
+
+	const script = SPECIAL_SCRIPT in packageJson.scripts ?
+		packageJson.scripts[SPECIAL_SCRIPT] : packageJson.scripts[FALLBACK_SCRIPT];
+
+	await $`yarn ${script}`;
 
 	await publishPackage(sha, packageJson);
 }
