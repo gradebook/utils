2a8a3e60fc81e0f3927b56f7043334d9b112a007
Vikas Potluri <vikaspotluri123.github@gmail.com>
2021-07-04 20:21:44 -0500

    fix api exports

--- packages/release-utils/src/index.ts
+++ packages/release-utils/src/index.ts
@@ -1,1 +1,5 @@
-export {default as parseChangelog} from './api/parse-changelog';
+export * from './api/actions-hook.js';
+export * from './api/configure-for-release.js';
+export * from './api/get-sha-from-env.js';
+export * from './api/parse-changelog.js';
+export * from './api/publish-package.js';
