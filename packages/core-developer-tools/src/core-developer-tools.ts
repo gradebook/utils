1edea9fd513b23b49adc3caef5f96733c4c2e9d7
Vikas Potluri <vikaspotluri123.github@gmail.com>
2021-11-01 23:47:58 -0500

    disallow loading core-developer-tools in prod

--- packages/core-developer-tools/src/core-developer-tools.ts
+++ packages/core-developer-tools/src/core-developer-tools.ts
@@ -1,6 +1,11 @@
 import type {Application} from 'express';
 import {register as registerSentry} from './tools/sentry.js';
 
+if (process.env.NODE_ENV === 'production') {
+	console.error('Attempted to load @gradebook/core-developer-tools in a production environment');
+	process.exit(1); // eslint-disable-line unicorn/no-process-exit
+}
+
 export function load(serverRoot: string, app: Application) {
 	registerSentry(app);
 }
