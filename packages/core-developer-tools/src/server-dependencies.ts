6cf49af70bf3386ff9cc006d34944718271efa13
Vikas Potluri <vikaspotluri123.github@gmail.com>
2022-02-26 18:26:36 -0600

    :sparkles: core-developer-tools: add auth middleware

---
+++ packages/core-developer-tools/src/server-dependencies.ts
@@ -0,0 +1,24 @@
+import path from 'path';
+import type ExpressDependency from 'express';
+
+export class ServerDependencies {
+	public knex: any;
+	public express: typeof ExpressDependency;
+
+	private serverRoot: string;
+
+	async init(serverRoot: string) {
+		this.serverRoot = serverRoot;
+		// eslint-disable-next-line @typescript-eslint/no-unsafe-assignment
+		this.knex = await this.import<any>('./lib/database/knex.js');
+		this.express = await this.import<typeof ExpressDependency>('node_modules/express/index.js');
+	}
+
+	async import<T>(serverRelativePath: string): Promise<T> {
+		const absolutePath = path.resolve(this.serverRoot, serverRelativePath);
+
+		return import(absolutePath) as Promise<T>;
+	}
+}
+
+export const serverDependencies = new ServerDependencies();
