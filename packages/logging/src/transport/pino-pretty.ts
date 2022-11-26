c6114b6c988d6215f94e9ce4f48a75dc6bb30699
Vikas Potluri <vikaspotluri123.github@gmail.com>
2022-11-26 10:33:31 -0800

    pretty: add ignition color mapping

--- packages/logging/src/transport/pino-pretty.ts
+++ packages/logging/src/transport/pino-pretty.ts
@@ -1,8 +1,17 @@
 import * as PinoPretty from 'pino-pretty';
 
+const ignitionColors = Object.entries({
+	trace: 'grey',
+	debug: 'grey',
+	info: 'cyan',
+	warn: 'magenta',
+	error: 'red',
+	fatal: 'inverse',
+}).map(([level, color]) => `${level}:${color}`).join(',');
+
 const createSafeLogger = (options: Partial<PinoPretty.PrettyOptions>) => PinoPretty.default({
 	// @ts-expect-error this is documented in the README, but not as part of the TS types.
-	customColors: 'message:white',
+	customColors: `message:white,${ignitionColors}`,
 	useOnlyCustomProps: false,
 	...options,
 });
