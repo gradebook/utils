fc15098139689b4a033df1f6365a713ccbc3e001
Vikas Potluri <vikaspotluri123.github@gmail.com>
2022-12-24 18:32:01 -0800

    fix test metrics for uncovered files

--- packages/logging/__tests__/logging.spec.js
+++ packages/logging/__tests__/logging.spec.js
@@ -1,1 +1,9 @@
-// @TODO
+/* eslint-disable no-unused-vars */
+
+// These files aren't realistic to test (for now) so we just import it to get proper coverage metrics.
+
+import * as loggingNoop from '../lib/logging.js';
+import * as transportNoop from '../lib/transport.js';
+import * as fileRotateNoop from '../lib/transport/pino-file-rotate.js';
+import * as prettyNoop from '../lib/transport/pino-pretty.js';
+/* eslint-enable no-unused-vars */
