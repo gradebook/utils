aa3f84f17931c29015b86855e4e6ca95176b7337
Vikas Potluri <vikaspotluri123.github@gmail.com>
2023-05-28 15:28:29 -0700

    fix(app-core): catch logger initialization errors and dump to stdout

--- packages/app-core/src/app-core.ts
+++ packages/app-core/src/app-core.ts
@@ -1,8 +1,20 @@
+import process from 'node:process';
+import util from 'node:util';
 import {getConfig} from '@gradebook/config';
 import {createConfig, createLogger} from '@gradebook/logging';
 
 export const config = await getConfig();
-export const logger = await createLogger(config.get('logging'));
 
+let loadedLogger;
+
+try {
+	loadedLogger = await createLogger(config.get('logging'));
+} catch (error) {
+	process.stderr.write('Failed creating logger:\n');
+	process.stdout.write(util.inspect(error));
+	process.stdout.write('\n');
+}
+
+export const logger = loadedLogger;
 // @todo: this config coercion happens 2x (once internally and once here) - can we limit it to 1x?
 export const $$loggingConfig = createConfig(config.get('logging'));
