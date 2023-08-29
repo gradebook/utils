9e704a353a856c3aeeb660b4753ad629107eaed9
Vikas Potluri <vikaspotluri123.github@gmail.com>
2023-08-29 07:15:55 -0700

    bootstrap empty errors package

---
+++ packages/errors/__tests__/errors.spec.js
@@ -0,0 +1,9 @@
+// @ts-check
+import {expect} from 'chai';
+import {errors} from '../../lib/errors.js';
+
+describe('Unit > errors', function () {
+	it('Hello, world', function () {
+		expect(errors()).to.equal('Hello from errors');
+	});
+});
