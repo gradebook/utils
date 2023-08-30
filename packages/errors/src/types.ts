58bae711bd9e7515c73bb76506221ff4e632f709
Vikas Potluri <vikaspotluri123.github@gmail.com>
2023-08-29 21:49:16 -0700

    implement @gradebook/errors
    based on tryghost/errors

---
+++ packages/errors/src/types.ts
@@ -0,0 +1,19 @@
+export type Level = 'normal' | 'critical';
+
+export interface ErrorOptions {
+	id?: string;
+	level?: Level;
+	statusCode?: number;
+	context?: unknown;
+	help?: string;
+	errorType?: string;
+	errorDetails?: unknown;
+	code?: string | null;
+	property?: string | null;
+	redirect?: string | null;
+	message?: string;
+	hideStack?: boolean;
+	/** @deprecated */
+	err?: string | Error | null;
+	error?: string | Error | null;
+}
