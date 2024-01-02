d84c29d3669216617dae904ad388fa5721a4864d
Vikas Potluri <vikaspotluri123.github@gmail.com>
2024-01-01 21:04:20 -0800

    trusted-request: add sanity tests

--- packages/trusted-request/__tests__/trusted-request.spec.js
+++ packages/trusted-request/__tests__/trusted-request.spec.js
@@ -1,5 +1,10 @@
-module.exports = trustedRequest;
+import {expect} from 'chai';
+import {allowTrustedIps, TrustedRequestError} from '../lib/trusted-request.js';
 
-function trustedRequest() {
-	// TODO
-}
+describe('Unit > Trusted Request', function () {
+	it('API', function () {
+		expect(allowTrustedIps).to.be.a('function');
+		expect(TrustedRequestError).to.be.a('function');
+		expect(Object.getPrototypeOf(TrustedRequestError)).to.equal(Error);
+	});
+});
