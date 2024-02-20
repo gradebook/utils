48f715691aaa9c3be1ec3d7a369588daf43e9a4f
Vikas Potluri <vikaspotluri123.github@gmail.com>
2024-02-20 12:45:46 -0800

    teleport: fix broken tests from organization
    - the lib folder wasn't cleaned so tests were passing locally until I rimrafed it

--- packages/teleport/__tests__/fixtures/user-export.js
+++ packages/teleport/__tests__/fixtures/user-export.js
@@ -1,10 +1,11 @@
+// @ts-check
 import path from 'node:path';
 import {readFile} from 'node:fs/promises';
 import {fileURLToPath} from 'node:url';
 
 const root = path.dirname(fileURLToPath(import.meta.url));
 
-/** @type {import('../../lib/exporter/raw').RawExportedUser} */
+/** @type {import('../../lib/shared/interfaces.js').RawExport} */
 export const rawUserExport = JSON.parse(
 	await readFile(
 		path.resolve(
@@ -15,6 +16,7 @@ export const rawUserExport = JSON.parse(
 	),
 );
 
+/** @type {import('../../lib/shared/interfaces.js').PublicExport} */
 export const publicUserExport = JSON.parse(
 	await readFile(
 		path.resolve(
