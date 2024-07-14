e68a789ba98070e6ca0635777776aebd3cfa0527
Ramsay Smith <ramsaysmith@outlook.com>
2024-07-14 16:34:49 -0500

    add semester class to browser

--- packages/time/src/browser.ts
+++ packages/time/src/browser.ts
@@ -5,6 +5,7 @@ dayjs.extend(cpf);
 
 export * as semester from './semester-calendar.js';
 export * as tzOffset from './tz-offset.js';
+export * as Semester from './semester.js';
 export {default as unitsToMs} from './units-to-ms.js';
 
 export {default as dayjs} from 'dayjs/esm/index.js';
