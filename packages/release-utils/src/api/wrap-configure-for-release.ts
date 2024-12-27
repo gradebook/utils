438e534b31522d771d0a89d536fbc7929fabec22
Vikas Potluri <vikaspotluri123.github@gmail.com>
2024-12-26 20:04:03 -0800

    release-utils: `wrapConfigureForRelease`: log cwd change

--- packages/release-utils/src/api/wrap-configure-for-release.ts
+++ packages/release-utils/src/api/wrap-configure-for-release.ts
@@ -1,7 +1,16 @@
+import {cwd} from 'process';
 import {configureForRelease} from './configure-for-release.js';
 
 export async function loudConfigureForRelease(commitOrTag: string) {
+	const originalWd = cwd();
 	const {packageJson, isMergeRef} = await configureForRelease(commitOrTag);
+
+	const newWd = cwd();
+
+	if (originalWd !== newWd) {
+		console.log(`cwd: ${cwd()}`);
+	}
+
 	if (isMergeRef) {
 		console.log(`${commitOrTag} is a merge commit, using parent tree`);
 	}
